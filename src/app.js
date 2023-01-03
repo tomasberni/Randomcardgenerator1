fdcf39b055e5d12660528e4fec21f1ac8aa4af26
T0MAS2 <tomasbernimozzo@gmail.com>
2023-01-03 17:53:56 +0000

    random card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
+let palo = ["♦", "♥", "♠", "♣"];
+let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let sorteoPalo = Math.floor(Math.random() * palo.length);
+let sorteoNumber = Math.floor(Math.random() * number.length);
+let contenedorNumber = number[sorteoNumber];
+let contenedorPalo = palo[sorteoPalo];
+let color = contenedorPalo == "♦" || contenedorPalo == "♥" ? "red" : "black";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.getElementById("arriba").innerHTML = contenedorPalo;
+document.getElementById("arriba").style.color = color;
+document.getElementById("medio").innerHTML = contenedorNumber;
+document.getElementById("medio").style.color = color;
+document.getElementById("abajo").innerHTML = contenedorPalo;
+document.getElementById("abajo").style.color = color;
